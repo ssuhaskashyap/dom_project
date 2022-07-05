b35a6fea348b062970853dc90fb963e6a6b60215
Suhas <kashyap.suhas@protonmail.com>
2022-07-06 00:15:53 +0530

    Project-4: Rendered the movie list item onto the UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,9 +4,17 @@ const backDrop = document.getElementById('backdrop');
 const mainModalCancel = mainModal.querySelector('.btn--passive');
 const mainModalAdd = mainModal.querySelector('.btn--success');
 const mainInputElements = mainModal.getElementsByTagName('input');
+const mainSection = document.getElementById('entry-text');
 let movies = [];
 
 // console.log(titleValue, imageUrlValue, ratingValue);
+const updateUi = () => {
+  if (movies.length === 0) {
+    mainSection.style.display = 'block';
+  } else {
+    mainSection.style.display = 'none';
+  }
+}
 
 const toggleMainModalHandler = () => {
   mainModal.classList.toggle("visible");
@@ -33,6 +41,7 @@ const addMainModalHandler = () => {
   const imageUrlValue = mainInputElements[1].value;
   const ratingValue = mainInputElements[2].value;
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue
@@ -47,6 +56,35 @@ const addMainModalHandler = () => {
 
   clearInputElements();
   toggleMainModalHandler();
+  renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
+  updateUi();
+}
+
+const renderNewMovieElement = (id, title, image, rating) => {
+  const listRoot = document.getElementById('movie-list');
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+  <div class="movie-element__image">
+    <img src="${image}" alt="${title}">
+  </div>
+  <div class="movie-element__info">
+    <h2>${title}</h2>
+    <p>${rating}/5</p>
+  </div>
+  `;
+
+  listRoot.append(newMovieElement);
+}
+
+const deleteMovieElement = (movieId) => {
+  let ind = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      movies.splice(ind, 1);
+    }
+    ind++;
+  }
 }
 
 startButton.addEventListener('click', toggleMainModalHandler);
