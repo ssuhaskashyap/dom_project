e582a6f766348e75979957b7cd30c88067805c3c
Suhas <kashyap.suhas@protonmail.com>
2022-07-06 14:15:42 +0530

    Project-4: complete movie database project

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,9 +5,12 @@ const mainModalCancel = mainModal.querySelector('.btn--passive');
 const mainModalAdd = mainModal.querySelector('.btn--success');
 const mainInputElements = mainModal.getElementsByTagName('input');
 const mainSection = document.getElementById('entry-text');
+const listRoot = document.getElementById('movie-list');
+const deleteModal = document.getElementById('delete-modal');
+const cancelDeleteModal = deleteModal.querySelector('.modal__actions').querySelector('.btn--passive');
+let confirmDeleteModal = deleteModal.querySelector('.modal__actions').querySelector('.btn--danger');
 let movies = [];
 
-// console.log(titleValue, imageUrlValue, ratingValue);
 const updateUi = () => {
   if (movies.length === 0) {
     mainSection.style.display = 'block';
@@ -16,8 +19,8 @@ const updateUi = () => {
   }
 }
 
-const toggleMainModalHandler = () => {
-  mainModal.classList.toggle("visible");
+const showMainModalHandler = () => {
+  mainModal.classList.add("visible");
   toggleBackDrop();
 }
 
@@ -26,7 +29,7 @@ const toggleBackDrop = () => {
 }
 
 const cancelMainModalHandler = () => {
-  toggleMainModalHandler();
+  removeMainModalHandler();
   clearInputElements();
 }
 
@@ -49,19 +52,26 @@ const addMainModalHandler = () => {
 
   if (titleValue.trim() === '' || imageUrlValue.trim() === '' || ratingValue.trim() === '' || +ratingValue < 1 || +ratingValue > 5) {
     alert('Please enter a valid entry');
+    return;
   } else {
     movies.push(newMovie);
-    console.log(movies);
   }
 
   clearInputElements();
-  toggleMainModalHandler();
+  removeMainModalHandler();
   renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
   updateUi();
 }
 
+const removeMainModalHandler = () => {
+  if (mainModal.className === 'modal visible') {
+    mainModal.classList.remove("visible");
+    clearInputElements();
+    toggleBackDrop();
+  }
+}
+
 const renderNewMovieElement = (id, title, image, rating) => {
-  const listRoot = document.getElementById('movie-list');
   const newMovieElement = document.createElement('li');
   newMovieElement.className = 'movie-element';
   newMovieElement.innerHTML = `
@@ -74,20 +84,45 @@ const renderNewMovieElement = (id, title, image, rating) => {
   </div>
   `;
 
+  newMovieElement.addEventListener('click', deleteMovieModal.bind(this, id));
   listRoot.append(newMovieElement);
 }
 
-const deleteMovieElement = (movieId) => {
+// https://www.mbatious.com/uploads/files/1497349879912-movies.jpg
+
+const deleteMovieModal = (index) => {
+  toggleBackDrop();
+  deleteModal.classList.add('visible');
+
+  confirmDeleteModal.replaceWith(confirmDeleteModal.cloneNode(true));
+  confirmDeleteModal = deleteModal.querySelector('.modal__actions').querySelector('.btn--danger');
+  cancelDeleteModal.removeEventListener('click', cancelDeleteModalHandler);
+
+  cancelDeleteModal.addEventListener('click', cancelDeleteModalHandler);
+  confirmDeleteModal.addEventListener('click', deleteMovieElementHandler.bind(null, index));
+}
+
+const cancelDeleteModalHandler = () => {
+  deleteModal.classList.remove('visible');
+  toggleBackDrop();
+}
+
+const deleteMovieElementHandler = (movieId) => {
+  deleteModal.classList.remove('visible');
+  toggleBackDrop();
   let ind = 0;
   for (const movie of movies) {
     if (movie.id === movieId) {
-      movies.splice(ind, 1);
+      break;
     }
     ind++;
   }
+  movies.splice(ind, 1);
+  listRoot.children[ind].remove();
+  updateUi();
 }
 
-startButton.addEventListener('click', toggleMainModalHandler);
-backDrop.addEventListener('click', toggleMainModalHandler);
+startButton.addEventListener('click', showMainModalHandler);
+backDrop.addEventListener('click', removeMainModalHandler);
 mainModalCancel.addEventListener('click', cancelMainModalHandler);
 mainModalAdd.addEventListener('click', addMainModalHandler);
